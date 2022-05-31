a30f09067ab85be285b65f1073a183fe5fa542e9
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-06-01 00:04:11 +0530

    stop after sometimes

--- react-all/src/Component/StopAfterSomeTime/StopAfterSomeTime.jsx
+++ react-all/src/Component/StopAfterSomeTime/StopAfterSomeTime.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const StopAfterSomeTime = () => {
-    
+export default function StopAfterSomeTime() {
+
+    const[sec, setSec] = useState(0)
+    const[min , setMin] = useState(0)
+    const[visible, setVisible] = useState(false)
+
+    useEffect(() => {
+
+        setTimeout(() => {
+           if(visible){
+            if(min == 2){
+                alert("done")
+                setMin(0)
+                setSec(0)
+                setVisible(false)
+                clearTimeout()
+            }
+            else{
+                if(sec == 60){
+                    setMin(min + 1)
+                    setSec(0)
+                }
+                else{
+                    setSec(sec + 1)
+                }
+            }
+           }
+
+            
+            
+        },200)
+
+    },[sec, min, visible])
+
   return (
     <div>
-      
+      <h1>Stop after sometimes .. !! </h1>
+      <h1>{min}</h1>
+      <h1>{sec}</h1>
+      <button onClick={() => setVisible(true) }>start</button>
     </div>
   )
 }
-
-export default StopAfterSomeTime
