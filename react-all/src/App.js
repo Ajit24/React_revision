a4223bc1cf35f30e6a58f57c83458f38cb5da95b
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-05-31 23:56:05 +0530

    stop after some times

--- react-all/src/App.js
+++ react-all/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 //import Test from './Component/Test/Test';
-import Validation from './Component/Validation/Validation'
+//import Validation from './Component/Validation/Validation'
+import StopAfterSomeTime from './Component/StopAfterSomeTime/StopAfterSomeTime';
 function App() {
   return (
     <div className="App">
       
       {/* <Test/> */}
-      <Validation />
+      {/* <Validation /> */}
+        <StopAfterSomeTime/>
+
     </div>
   );
 }
