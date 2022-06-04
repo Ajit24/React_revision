32e295ef426c052317995c2e42a7ea6b88ed6b41
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-06-04 13:40:30 +0530

    time clock

--- react-all/src/Component/TimeClock/TimeClock.jsx
+++ react-all/src/Component/TimeClock/TimeClock.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './TimeClock.css'
+import {useState,useEffect} from "react"
+
 
 
 const TimeClock = () => {
+
+    var time =[];
+   time = new Date().toLocaleTimeString();
+   const [ctime,setCtime] = useState(time)
+  
+     setInterval(()=>{
+      var time =[];
+      time = new Date().toLocaleTimeString();
+       setCtime(ctime)
+     },1000)
+
+   
+  
+  console.log(time)
+
   return (
     <div>
         <h2>Time Clock .. !!</h2>
-
+    
+       <h1>{time}</h1>
+    
 
     </div>
   )
