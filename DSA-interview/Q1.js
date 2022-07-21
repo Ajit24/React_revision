57789ef30d257bd410b06e7720e5aef41460bcfc
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-21 23:54:54 +0530

    second largest in array

--- DSA-interview/Q1.js
+++ DSA-interview/Q1.js
@@ -68,3 +68,37 @@ function findMax(arr) {
   return max;
 }
 console.log("findMax(arr):", findMax(arr));
+
+// second largest in array 
+
+function secondLargest(arr) {
+
+  var max1st = arr[0];
+
+  var max2nd = 0;
+
+  
+  for(var i=0; i<arr.length; i++) {
+
+    if(arr[i] > max1st) {
+
+      max2nd = max1st;
+
+      max1st = arr[i];
+
+    }
+
+    else if(arr[i] > max2nd && arr[i] !== max1st) {
+
+      max2nd = arr[i];
+
+    }
+
+  }
+
+  return max2nd;
+
+}
+
+
+console.log(secondLargest([100,2,4,54,27,98,99]));
