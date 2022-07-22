a8efdac7b09afd755f440542b45012cb54729635
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-22 22:35:20 +0530

    printFirstRepeating

--- DSA-interview/import.js
+++ DSA-interview/import.js
@@ -24,23 +24,23 @@
 //aa11b - a211b1
 // Ab&& - a1b1&&
 
-let str = "AabbB11cbbd";
-let arr = str.toLocaleLowerCase();
-let obj = {};
-for (let i = 0; i < arr.length; i++) {
-  if (obj[arr[i]] == undefined) {
-    obj[arr[i]] = 1;
-  } else if ((obj[arr[i]] = 1)) {
-    obj[arr[i]]++;
-  }
-}
-for (let key in obj) {
-  if (typeof key == "number") {
-    console.log(key);
-  } else {
-    console.log(key, obj[key]);
-  }
-}
+// let str = "AabbB11cbbd";
+// let arr = str.toLocaleLowerCase();
+// let obj = {};
+// for (let i = 0; i < arr.length; i++) {
+//   if (obj[arr[i]] == undefined) {
+//     obj[arr[i]] = 1;
+//   } else if ((obj[arr[i]] = 1)) {
+//     obj[arr[i]]++;
+//   }
+// }
+// for (let key in obj) {
+//   if (typeof key == "number") {
+//     console.log(key);
+//   } else {
+//     console.log(key, obj[key]);
+//   }
+// }
 
 // Given an integer n, write program to print following pattern
 // N = 5:
@@ -84,4 +84,85 @@ for (let key in obj) {
 // }
 // var a = "aabbddd";
 // console.log(counter(a));
+
+// --------------------------- DSA QUES ----------------------------
+
+//var arr = [2,5,4,2,1,2,6];
+// out = 5;
+
+// PRINT REPEATING NUMBERS IN GIVEN ARRAY
+
+//  let  array = [ 0, 4, 3, 2, 7, 8, 2, 3, 1,];
+// //output = [ 2, 3 ]
+//     let n =  array.length;
+//     var ans = []
+    
+    
+//     // count the frequency
+//     for (let i = 0; i < n; i++) {
+//          array[ array[i] % n]
+//             =  array[ array[i] % n] + n;
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         if ( array[i] >= n * 2) {
+//             ans.push(i)
+//         }
+//     }
+//     console.log(ans);
+
+   // ----------  printFirstRepeating ------------------------
+
+   var arr = [ 10, 5, 3, 4, 3, 5, 6 ];
+  // out = 5;
+   var n = arr.length;
+   function printFirstRepeating(arr , n) {
+
+       // This will set k=1, if any
+       // repeating element found
+       var k = 0;
+   
+       // max = maximum from (all elements & n)
+       var max = n;
+       for (i = 0; i < n; i++)
+           if (max < arr[i])
+               max = arr[i];
+   
+       // Array a is for storing
+       // 1st time occurrence of element
+       // initialized by 0
+       var a = Array(max + 1).fill(0);
+   
+       // Store 1 in array b
+       // if element is duplicate
+       // initialized by 0
+       var b = Array(max + 1).fill(0);
+       for (var i = 0; i < n; i++) {
+   
+           // Duplicate element found
+           if (a[arr[i]] != 0) {
+               b[arr[i]] = 1;
+               k = 1;
+               continue;
+           } else
+               // storing 1st occurrence of arr[i]
+               a[arr[i]] = i+1;
+       }
+   
+       if (k == 0)
+           console.log("No repeating element found");
+       else {
+           var min = max + 1;
+   
+           // trace array a & find repeating element
+           // with min index
+           for (i = 0; i < max + 1; i++)
+               if (a[i] != 0 && min > a[i] && b[i] != 0)
+                   min = a[i];
+           console.log(arr[min-1]);
+       }
+   }
+       printFirstRepeating(arr, n);
+       //output = 5
+
 
