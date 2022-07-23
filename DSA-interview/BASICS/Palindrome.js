e06966581a6cb7dc2251bee8e74aa370aa8fb5be
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-07-24 00:06:34 +0530

    dsa

---
+++ DSA-interview/BASICS/Palindrome.js
@@ -0,0 +1,40 @@
+// check Palindrome
+//var str = nrupul;
+function checkPalindrome(str) {
+
+    //write code here
+   // var n = str.length;
+    for(var i = 0; i<str.length; i++){
+        if(str[i]!= str[str.length-1]){
+            console.log("No");
+            break;
+        }else{
+            console.log("Yes");
+            break;
+        }
+    }
+  
+}
+checkPalindrome(str);
+
+// 2.
+
+// function detectPalindrome(num){
+//     //write code here
+//     var num = 1221;
+    
+//     for(var i = 0; i<num.length; i++){
+//         if(num[i] != num[num.length - 1]){
+//             console.log("No");
+//             break;
+//         }else {
+//             console.log("Yes");
+//             break;
+//         }
+//     }
+    
+    
+    
+    
+//     }
+    
---
+++ DSA-interview/BASICS/reverseStrr.js
@@ -0,0 +1,37 @@
+// Reverse a String With Recursion
+
+function reverseString(str) {
+    if (str === "")
+      return "";
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  }
+ console.log(reverseString("hello"));
+ // out = olleh
+
+ // Conditional (Ternary) Operator:
+ function reverseString(str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+  }
+  reverseString("hello");
+
+  
+
+ // 2. Reverse a String With a Decrementing For Loop
+
+ function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+
+//3.  Reverse a String With Built-In Functions
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
+
