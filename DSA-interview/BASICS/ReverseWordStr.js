fb204f352d081083f5e205303c02ac333631705d
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-10 12:56:48 +0530

    reverse str word

--- DSA-interview/BASICS/ReverseWordStr.js
+++ DSA-interview/BASICS/ReverseWordStr.js
@@ -1,4 +1,5 @@
 const str = '123 Hello World how is it outside';
+//           321 olleH dlroW woh si ti edistuo
 const reverseSentence = str => {
    const arr = str.split(" ");
    //console.log(arr)
