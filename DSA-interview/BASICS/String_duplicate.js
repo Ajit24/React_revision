a71489031993ab4bd4c117e74e37aaed5eba44df
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-11 23:47:36 +0530

    duplicate in string

--- DSA-interview/BASICS/String_duplicate.js
+++ DSA-interview/BASICS/String_duplicate.js
@@ -12,6 +12,9 @@
 // false
 // true
 
+// Time Complexity: O(n).
+// Space Complexity: O(n).
+
 // ------------   For incase-sensitive letters
 
 let containsDuplicates = (str) => {
@@ -44,3 +47,28 @@ console.log(containsDuplicates("I Love learnersbucket"));
 // true
 // false
 // true
+
+// -------------  For case-sensitive letters
+
+let containsDuplicates2 = (str) => {
+    let set = new Set();
+    
+    //If it is not whitespace the check if aplhabet is present or not
+    for(let i = 0; i < str.length; i++){
+  
+     if(str[i] != ' '){
+  
+      if(set.has(str[i])){
+        return true;
+      }
+  
+       set.add(str[i]);
+      }
+    }
+    
+    return false;
+  }
+
+console.log(containsDuplicates2("learnersbucket"));
+console.log(containsDuplicates2("abcdefghijk"));
+console.log(containsDuplicates2("I Luv life"));
