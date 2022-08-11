4498f42df6185c2c0f950bcb0320bf86edab1c21
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-11 16:37:13 +0530

    check duplicate in stttrrrr

---
+++ DSA-interview/BASICS/String_duplicate.js
@@ -0,0 +1,13 @@
+// An algorithm to check if there are duplicate letters in the string.
+
+// We will check if the given string has duplicate letters and if found we will return true, false otherwise.
+
+// Input:
+// 'learnersbucket'
+// 'abcdefghijk'
+// 'I Love learnersbucket'
+
+// Output:
+// true
+// false
+// true
