a8ed80c32e10b2b217c9fc9b054a0b3c8e6e6fba
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-23 20:45:12 +0530

    prime num

--- DSA-interview/BASICS/PrimeNo.js
+++ DSA-interview/BASICS/PrimeNo.js
@@ -46,3 +46,33 @@ function twoPrimes(a,b) {
 }
 twoPrimes(11,17)
 
+// // program to print prime numbers between the two numbers
+
+
+// program to print prime numbers between the two numbers
+
+// take input from the user
+const lowerNumber = 2;
+const higherNumber = 10;
+// 2 3 5 7
+
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+// looping from lowerNumber to higherNumber
+for (let i = lowerNumber; i <= higherNumber; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
+
