0dd0b28f2cb8f9f6e0f1bbd77489ccce1f0b6879
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-28 00:28:45 +0530

    palindrome.js

--- DSA-interview/BASICS/Palindrome.js
+++ DSA-interview/BASICS/Palindrome.js
@@ -1,40 +1,35 @@
-// check Palindrome
-//var str = nrupul;
-function checkPalindrome(str) {
+let str = "civic";
+let strReverse = str.split("").reverse().join("");
 
-    //write code here
-   // var n = str.length;
-    for(var i = 0; i<str.length; i++){
-        if(str[i]!= str[str.length-1]){
-            console.log("No");
-            break;
-        }else{
-            console.log("Yes");
-            break;
-        }
-    }
-  
+if (str === strReverse) {
+  console.log("YES");
+} else {
+  console.log("NOT");
 }
-checkPalindrome(str);
 
-// 2.
+//  ----------- number 
 
-// function detectPalindrome(num){
-//     //write code here
-//     var num = 1221;
+var isPalindrome = function(x) {
     
-//     for(var i = 0; i<num.length; i++){
-//         if(num[i] != num[num.length - 1]){
-//             console.log("No");
-//             break;
-//         }else {
-//             console.log("Yes");
-//             break;
-//         }
-//     }
+	// because reversed number will have '-' sign at the end 
+	// and original will have it at the front.
+    if(x < 0) return false;
     
+    let num = x;
     
+    let reversedNum = 0;
     
+    while(num > 0){
+        
+        reversedNum = reversedNum*10 + num%10;
+        
+		// return only the part before the decimal.
+        num = Math.trunc(num / 10);
+        
+    }
     
-//     }
+    return reversedNum == x;
     
+};
+console.log(isPalindrome(121));  // true
+  
