13beca3fc610567144a40e97b77f89000a19911d
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-28 16:28:38 +0530

    fibb recursive

--- DSA-interview/BASICS/fibonacci.js
+++ DSA-interview/BASICS/fibonacci.js
@@ -66,3 +66,16 @@ else {
 }
 // 0 1 1 2 3 
 
+// - ----------------------    Recursive Fibonacci Sequence
+
+function recursiveFibonacci(n) {
+    if (n < 2) {
+      return n
+    }
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
+  }
+  
+  console.log(recursiveFibonacci(0)) // 0
+  console.log(recursiveFibonacci(1)) // 1
+  console.log(recursiveFibonacci(7)) // 8
+
