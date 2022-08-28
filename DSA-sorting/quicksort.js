6ce2e529e5ac2927fd9f817ebb15567c0069adb9
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-28 16:19:07 +0530

    QUICK-SORT

--- DSA-sorting/quicksort.js
+++ DSA-sorting/quicksort.js
@@ -1,50 +1,17 @@
-function findit(array, l, h) {
-if( l < h){
-  let pivot = partition(array,l,h)
-  findit(array,l , pivot-1)
-  findit(array,pivot+1,h)
-}
-}
-function partition(array, l, h) {
-  let pivot = array[l];
-  let i = l,
-    j = h;
-  while (i < j) {
-    while (array[i] <= pivot) i++;
-    while (array[j] > pivot) j--;
-    if (i < j) {
-      [array[j], array[i]] = [array[i], array[j]];
+function quickSort(arr) {
+  if (arr.length < 2) return arr
+  let pivot = arr[arr.length - 1]
+  let left = []
+  let right = []
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
     }
   }
-  [array[j], array[l]] = [array[l], array[j]];
-  return j;
+  return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
-function runProgram(input) {
-  input = input.trim().split("\n");
-  let n = +input[0];
-  let array = input[1].trim().split(" ").map(Number);
-  findit(array, 0, 4)
-  console.log(array);
-}
-if (process.env.USERNAME === "Ajit") {
-  runProgram(`5
-      3 5 0 9 8`);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-}
+const arr = [8, 20, -2, 4, -6]
+console.log(quickSort(arr)) // [-6, -2, 4, 8, 20]
