655ce56f9fdbfd09ca359aa2c19a2fd55b9f300e
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-28 16:34:50 +0530

    Prime.js

--- DSA-interview/BASICS/prime.js
+++ DSA-interview/BASICS/prime.js
@@ -1,3 +1,30 @@
+// check prime 
+function isPrime(n) {
+    if (n < 2) {
+      return false
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      // if n is divisible by any number then it is not prime
+      if (n % i === 0) {
+        return false
+      }
+    }
+    return true
+  }
+  
+  console.log(isPrime(1)) // false
+  console.log(isPrime(5)) // true
+  console.log(isPrime(4)) // false
+
+
+
+
+
+
+
+
+
+
 function isPrime(n) {
     if (n == 1)
         return false;
