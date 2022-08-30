54996f2db4f4bed09c7b2c7c554e049dc2623937
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-30 13:38:17 +0530

    longestWord/length

--- DSA-interview/BASICS/longestWord.js
+++ DSA-interview/BASICS/longestWord.js
@@ -12,3 +12,18 @@ function longestWord(string) {
 }
 console.log(longestWord("pride and prejudice"));  
 //prejudice
+
+// ------------ 2nd sol  --------- 
+// it will print longest word length ;
+
+function findLongestWord(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+      longestWord = strSplit[i].length;
+       }
+    }
+    return longestWord;
+  }
+ console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); //6
