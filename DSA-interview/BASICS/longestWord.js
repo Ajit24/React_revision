22e9444e1cb0a4109f4603df6910dd24eab607dc
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-30 13:40:09 +0530

    longst-word/length in string

--- DSA-interview/BASICS/longestWord.js
+++ DSA-interview/BASICS/longestWord.js
@@ -13,7 +13,28 @@ function longestWord(string) {
 console.log(longestWord("pride and prejudice"));  
 //prejudice
 
-// ------------ 2nd sol  --------- 
+// -------------------- 2nd sol
+const str = 'Coding in JavaScript is really fun';
+const findLongest = (str = '') => {
+   const strArr = str.split(' ');
+   const word = strArr.reduce((acc, val) => {
+      let { length: len } = acc;
+      if(val.length > len){
+         acc = val;
+      };
+      return acc;
+   }, '');
+   return word;
+};
+console.log(findLongest(str));
+//JavaScript
+
+
+
+
+
+
+// ------------ 3rdd sol  --------- 
 // it will print longest word length ;
 
 function findLongestWord(str) {
