caeeb97180c61388b92f337a8c87da26959c9acf
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-31 15:05:05 +0530

    running server

--- NODEJS/index.js
+++ NODEJS/index.js
@@ -1,17 +1,12 @@
-// var http = require('http');
-
-// http.createServer(function(req,res){
-//     res.write("Hello Ajit");
-//     res.end();
-// }).listen(8800);
-
 // include http module in the file
 var http = require('http');
  
-// create a server listening on 8087
+// create a server
 http.createServer(function (req, res) {
-    // write the response and send it to the client
+    // http header
+    // 200 - is the OK message
+    // to respond with html content, 'Content-Type' should be 'text/html'
     res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('Node.js says hello!');
-    res.end();
-}).listen(8087);
+    res.write('Node.js says hello!'); //write a response to the client
+    res.end(); //end the response
+}).listen(9000); //the server object listens on port 9000
