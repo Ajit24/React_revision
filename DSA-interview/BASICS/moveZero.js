5e23e2ccb16d65eb7a93e2ddadc36176337fed6c
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-09-01 18:22:59 +0530

    move zero to the end

--- DSA-interview/BASICS/moveZero.js
+++ DSA-interview/BASICS/moveZero.js
@@ -1,28 +1,34 @@
-// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-// Input: [0,1,0,3,12] Output: [1,3,12,0,0]
-
-
-//var nums = [0,1,0,3,12];
-
-const moveZeroes = nums => {
-    if (nums.length<=1)
-        return
-
-    let indexZero = nums.indexOf(0);
-    let i = indexZero+1
-
-    while (indexZero<nums.length && i<nums.length && indexZero !== -1){
-
-        if (nums[i] !== 0){ // swap
-            nums[indexZero] = nums[i]
-            nums[i] = 0
-            indexZero++
-            i = indexZero+1 // reset
-        } else {
-            i++
-        }
-    }
-    console.log(moveZeroes([0,1,0,3,12]));
+const arr = [5, 0, 1, 0, -3, 0, 4, 6];
+const moveAllZero = (arr) => {
+   const res = [];
+   let currIndex = 0;
+   for(let i = 0; i < arr.length; i++){
+      const el = arr[i];
+      if(el === 0){
+         res.push(0);
+      }else{
+         res.splice(currIndex, undefined, el);
+         currIndex++;
+      };
+   };
+   return res;
 };
-// console.log(moveZeroes([0,1,0,3,12]));
+console.log(moveAllZero(arr));
+
+// guaranteed to has O(N) complexity
+function moveZerosToEndFast(a) {
+    if (a.length <= 1) { return a; }
+    
+    let zi = a.indexOf(0); // O(N)
+    if (zi === -1 || zi === a.length - 1) { return a; }
+    
+    for(let i = zi + 1; i < a.length; i++) {
+        if (a[i] !== 0) {
+            a[zi++] = a[i];
+            a[i] = 0;
+        }
+    } // O(N)
+    
+    return a;
+}
+console.log(moveZerosToEndFast([0,1,2,0,5,6,0])); // 1 2 5 6 0 0 0
