43d4b740486432ee8ded5a7e9c46dccddadede63
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-09-05 16:05:39 +0530

    reverse-str-words

--- DSA-interview/BASICS/ReverseStrWord.js
+++ DSA-interview/BASICS/ReverseStrWord.js
@@ -9,3 +9,18 @@ const reverseSentence = str => {
    return reversed.join(" ");
 };
 console.log(reverseSentence(str))
+
+// ------------ return the string in reversed order.
+
+// "Hello World and Coders"
+// sredoC dna dlroW olleH
+
+function FirstReverse(str) { 
+   var reverse = '';
+       for(var i = str.length - 1; i >= 0; i--)
+       reverse += str[i];
+       return reverse; 
+     return str; 
+            
+   }
+   console.log(FirstReverse("Hello World and Coders"));
