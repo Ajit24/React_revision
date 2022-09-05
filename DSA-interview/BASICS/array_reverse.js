58e616099bb17c19c0de02b4a92cf18b257971aa
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-09-05 17:25:00 +0530

    array-reverse

--- DSA-interview/BASICS/array_reverse.js
+++ DSA-interview/BASICS/array_reverse.js
@@ -2,7 +2,7 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for (let i = 0; i < arr.length / 2; i++) {
+for (let i = 0; i < arr.length/2 ; i++) {
   let tempFirst = arr[i]
   let tempLast = arr[arr.length - i - 1]
   arr[arr.length - i - 1] = tempFirst
