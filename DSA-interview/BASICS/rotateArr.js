2b4b7cda3947a8536f33ffd4199aa04f3947bfcf
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-09-09 14:15:17 +0530

    rotate arr

--- DSA-interview/BASICS/rotateArr.js
+++ DSA-interview/BASICS/rotateArr.js
@@ -19,3 +19,20 @@ for (let i = 0; i < arr.length;i++)
     temp[i] = arr[(i + arr.length-k) % arr.length];
 }
 console.log(temp);
+
+// 5 4
+//1 2 3 4 5 ----- left roattion
+
+function rotLeft(a, d) {
+    // Write your code here
+let r = []
+
+  d %= a.length
+
+  for (let i = 0; i < a.length; ++i) {
+    r[i] = a[(i + d) % a.length]
+  }
+
+  return r
+}
+console.log(rotLeft([1,2,3,4,5], 4))// 5 1 2 3 4
